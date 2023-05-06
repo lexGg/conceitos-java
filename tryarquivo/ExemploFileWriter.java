9b98ae28b810bcc7d27c66a31353cda029f9a0d8
lexGg <ribeiro2735@gmail.com>
2023-05-06 15:57:47 +0000

    adicionado escrita de arquivo exemplo

---
+++ tryarquivo/ExemploFileWriter.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExemploFileWriter {
+    public static void main(String[] args) {
+        // Texto a ser escrito no arquivo
+        String texto = "Olá, mundo!";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("arquivo.txt"))) {
+            // Cria um BufferedWriter, que recebe um FileWriter para escrever no arquivo "arquivo.txt"
+            // O uso do try-with-resources garante que o BufferedWriter será fechado automaticamente
+
+            // Escreve o texto no arquivo
+            writer.write(texto);
+        } catch (IOException e) {
+            // Trata a exceção de IO, caso ocorra algum erro na escrita do arquivo
+            System.out.println("Ocorreu um erro ao tentar escrever no arquivo: " + e.getMessage());
+        }
+    }
+}
